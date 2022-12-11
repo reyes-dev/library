19c7aee74102fc83dc8479ec8ba5c27289fed030
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-12-11 01:29:23 -0800

    Add function to loop through library and display books on page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let libraryContainer = document.querySelector('.libraryContainer')
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -10,3 +11,22 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook)
 }
+
+function displayBooks(myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let ul = document.createElement('ul')
+        
+        for(const prop in myLibrary[i]) {
+            li = document.createElement('li')
+            li.innerHTML = myLibrary[i][prop]
+            ul.appendChild(li)
+        }
+        
+        libraryContainer.appendChild(ul)
+    }
+}
+
+addBookToLibrary(new Book('The Crumpet', 'Kingsworth', 222, true))
+addBookToLibrary(new Book('Sinful Pride', 'Coia Lucioa', 123, false))
+addBookToLibrary(new Book('Orange of Wrath', 'Gulliver Smith', 84, true))
+displayBooks(myLibrary)
