6c71c8347854e4db63410699d29dfcfbc73df89e
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-12-11 03:26:30 -0800

    Add a button on each book’s display to remove the book from the library.

--- script.js
+++ script.js
@@ -41,18 +41,27 @@ function displayBooks(myLibrary) {
 
     for (let i = 0; i < myLibrary.length; i++) {
         let ul = document.createElement('ul')
-        
+        ul.setAttribute("data-libraryIndex", `${i}`)
+
         for(const prop in myLibrary[i]) {
             li = document.createElement('li')
             li.innerHTML = myLibrary[i][prop]
             ul.appendChild(li)
         }
         
+        let deleteBtn = document.createElement('button')
+        deleteBtn.onclick = function() {
+            deleteBtn.parentElement.remove()
+            myLibrary.splice(i, 1)
+            displayBooks(myLibrary)
+        }
+        deleteBtn.innerHTML = 'Delete Book'
+        ul.appendChild(deleteBtn)
         libraryContainer.appendChild(ul)
     }
 }
 
-addBookToLibrary(new Book('The Crumpet', 'Kingsworth', 222, true))
+addBookToLibrary(new Book('The Crumpet', 'Kingsworth McGee', 222, true))
 addBookToLibrary(new Book('Sinful Pride', 'Coia Lucioa', 123, false))
 addBookToLibrary(new Book('Orange of Wrath', 'Gulliver Smith', 84, true))
 displayBooks(myLibrary)
